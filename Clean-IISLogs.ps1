1969b23ba8e2c9b7a12a105058684a606ae99b5e
Sam Erde <20478745+SamErde@users.noreply.github.com>
2023-12-22 08:10:08 -0500

    🆕 Start writing script to clean IIS logs

---
+++ Clean-IISLogs.ps1
@@ -0,0 +1,54 @@
+# If the WebAdministration module is installed:
+$WebSites = Get-WebSite
+foreach ($item in $WebSites) {
+    $LogFileFolder = [string]($($WebSite.logFile.directory)\w3svc$($WebSite.id)).replace("%SystemDrive%",$env:SystemDrive)
+
+    # Create verbose and info output for this step
+    Remove-OldFiles -Path $LogFileFolder -Days 60
+}
+
+# Alternative approach to test
+$WebSites = Get-ChildItem IIS:\Sites
+foreach ($item in $WebSites) {
+    $LogFileFolder = $($item.logfile.directory) + "\w3svc" + $($item.id)
+
+    # Create verbose and info output for this step
+    Remove-OldFiles -Path $LogFileFolder -Days 60
+}
+
+# Check default log file location without requiring the WebAdministration module
+$DefaultIISLogLocation = "$env:SystemDrive\inetpub\logs\LogFiles"
+if (Test-Path -Path $DefaultIISLogLocation) {
+    Remove-OldFiles -Path $DefaultIISLogLocation -Days 60
+}
+
+# Check registry for log file lcoation without requiring the WebAdministration module
+$LogDir = Get-ItemProperty -Path "HKLM:\\System\\CurrentControlSet\\Services\\InetInfo\\Parameters" -Name "LogDir" | Select-Object -ExpandProperty LogDir
+if (Test-Path -Path $LogDir) {
+    Remove-OldFiles -Path $DefaultIISLogLocation -Days 60
+} else {
+    # Add some info and verbose messaging
+}
+
+function Remove-OldFiles {
+    [CmdletBinding()]
+    param (
+        [parameter]
+        $Path,
+
+        [parameter]
+        [int]$Days = 60
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        Get-ChildItem -Path $Path | Where-Object {$_.CreationTime -le ([datetime]::Now.AddDays( -$Days ))} | Remove-Item
+    }
+    
+    end {
+        
+    }
+}
