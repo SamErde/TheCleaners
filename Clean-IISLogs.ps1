e175ffacf25009bf8b92fe3f337326bf6a720d0b
Sam Erde <20478745+SamErde@users.noreply.github.com>
2023-12-22 08:13:02 -0500

    Add some CBH

--- Clean-IISLogs.ps1
+++ Clean-IISLogs.ps1
@@ -1,3 +1,27 @@
+<#
+    .SYNOPSIS
+    A script to clean out old IIS log files.
+
+    .DESCRIPTION
+    This is a work in progress still. It will attempt to find IIS log folders and clean out files older than [x] days. 
+    It checks the default log folder locations first, and if the WebAdministration PowerShell module is available, it 
+    will use that to check the specific log file locations for each web site as well.
+#>
+
+# Check default log file location without requiring the WebAdministration module
+$DefaultIISLogLocation = "$env:SystemDrive\inetpub\logs\LogFiles"
+if (Test-Path -Path $DefaultIISLogLocation) {
+    Remove-OldFiles -Path $DefaultIISLogLocation -Days 60
+}
+
+# Check registry for log file lcoation without requiring the WebAdministration module
+$LogDir = Get-ItemProperty -Path "HKLM:\\System\\CurrentControlSet\\Services\\InetInfo\\Parameters" -Name "LogDir" | Select-Object -ExpandProperty LogDir
+if (Test-Path -Path $LogDir) {
+    Remove-OldFiles -Path $DefaultIISLogLocation -Days 60
+} else {
+    # Add some info and verbose messaging
+}
+
 # If the WebAdministration module is installed:
 $WebSites = Get-WebSite
 foreach ($item in $WebSites) {
@@ -16,19 +40,6 @@ foreach ($item in $WebSites) {
     Remove-OldFiles -Path $LogFileFolder -Days 60
 }
 
-# Check default log file location without requiring the WebAdministration module
-$DefaultIISLogLocation = "$env:SystemDrive\inetpub\logs\LogFiles"
-if (Test-Path -Path $DefaultIISLogLocation) {
-    Remove-OldFiles -Path $DefaultIISLogLocation -Days 60
-}
-
-# Check registry for log file lcoation without requiring the WebAdministration module
-$LogDir = Get-ItemProperty -Path "HKLM:\\System\\CurrentControlSet\\Services\\InetInfo\\Parameters" -Name "LogDir" | Select-Object -ExpandProperty LogDir
-if (Test-Path -Path $LogDir) {
-    Remove-OldFiles -Path $DefaultIISLogLocation -Days 60
-} else {
-    # Add some info and verbose messaging
-}
 
 function Remove-OldFiles {
     [CmdletBinding()]
