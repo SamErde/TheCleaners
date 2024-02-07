4e08a45829f369d7fb936638c1670babd214b1fa
Sam Erde <20478745+SamErde@users.noreply.github.com>
2024-02-07 09:46:02 -0500

    Initial rename and revamp of functions

--- src/TheCleaners/Public/Clean-LocalProfiles.ps1
+++ src/TheCleaners/Public/Clean-LocalProfiles.ps1
@@ -1,55 +1,72 @@
-<#  Clean-LocalProfiles.ps1
+# THIS IS AN OLD POC SCRIPT THAT NEEDS TO BE SORTED AND TESTED
 
-    A script to find and remove old, unused user profiles in Windows. 
-    Exclude special accounts and profiles that should not be removed. 
-    Partially inspired by http://woshub.com/delete-old-user-profiles-gpo-powershell/
+function Get-StaleUserProfiles {
+<#
+    .SYNOPSIS
+        A script to find old, unused user profiles in Windows.
+
+    .DESCRIPTION
+        This script finds old, unused profiles in Windows and helps you remove them. It should exclude special accounts
+        and system profiles.
+
+    .NOTES
+        Author: Sam Erde
+                https://twitter.com/SamErde
+                https://github.com/SamErde
+
+        Partially inspired by http://woshub.com/delete-old-user-profiles-gpo-powershell/
 #>
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [switch]
+        $ShowSummary
+    )
 
-# Get all user profiles that have not been used in 60 days, are not currently loaded, and are not special accounts.
-$UserProfiles = Get-CimInstance -Class Win32_UserProfile | `
-    Where-Object { ($_.LastUseTime -lt (Get-Date).AddDays(-60)) -and (!$_.Special) -and (!$_.Loaded) }
+    # Get all user profiles that have not been used in 60 days, are not currently loaded, and are not special accounts.
+    [array]$StaleUserProfiles = Get-CimInstance -Class Win32_UserProfile | `
+        Where-Object { ($_.LastUseTime -lt (Get-Date).AddDays(-60)) -and (!$_.Special) -and (!$_.Loaded) }
 
-    If ($UserProfiles.Count -lt 1) { Write-Host "No applicable user profiles were found." -ForegroundColor Yellow }
-    Else { 
-        $UserProfiles | Select-Object LocalPath,SID, `
-            @{Name="Size";Expression={"{0} MB" -f [math]::Round(((Get-ChildItem $_.LocalPath -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB))}}
-        Write-Host $UserProfiles "`nRun the function CleanProfiles to remove these objects. Note, if you do not have access to a profile folder, the size will show as 0 MB." 
-    } # End if/else
+        if ($StaleUserProfiles.Count -lt 1 -or !(StaleUserProfiles)) {
+            Write-Information "No stale user profiles were found." -InformationAction Continue
+        }
+        else {
+            if ($ShowSummary) {
+                $StaleUserProfiles | Select-Object LocalPath,SID, `
+                    @{
+                        Name="Size";
+                        Expression={"{0} MB" -f [math]::Round(((Get-ChildItem $_.LocalPath -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB))}
+                    } | Out-Host
+                Write-Information -InformationAction Continue "NOTE: If you do not have access to a profile folder, the size will show as 0 MB."
+            }
+            $StaleUserProfiles
+        }
+}
 
-Function TranslateSamToSid ($domain, $samaccountname) 
-{
-    # Translate samaccountname to SID; use if getting additional account information from AD, such as special group membership
-    $objUser = New-Object System.Security.Principal.NTAccount($domain,$samaccountname)
-    $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
-    Return $strsid.Value
-} # End function
+$StaleUserProfiles = Get-StaleUserProfiles -ShowSummary
 
-Function TranslateSidtoSam ($sid) 
-{
-    # Translate SID to samaccountname; use if getting additional account information from AD, such as special group membership
-    $objSID = New-object System.Security.Principal.SecurityIdentifier($sid)
-    $strUser = $objSID.Translate([System.Security.Principal.NTAccount])
-    Return $strUser.Value
-} # End function
 
-Function CleanProfiles 
-{   
-    <# Need to finish with check to see if the account is a member of special domain groups, such as: 
-        Service Accounts
-        URA Groups
-        Admin Groups
-        IIS* Groups
+
+function Remove-StaleProfiles {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        $StaleUserProfiles
+    )
+    <#
+        May need to finish with a check to see if the account is a member of special domain groups. Are there
+        any domain groups whose users that might need to be left even if they appear to be inactive?
     #>
-    
+
     # Walk away if there are no profiles to clean out.
-    if ($UserProfiles.Count -lt 1) { 
+    if ($StaleUserProfiles.Count -lt 1) {
         Write-Host "There are no profiles to clean out." -ForegroundColor Yellow
-        Exit
+        break
     } # Only continue if there ARE profiles to clean up.
 
     $AlwaysExcluded ="Public","svc","admin" #Etc
 
-    foreach ($UserProfile in $UserProfiles)
+    foreach ($UserProfile in $StaleUserProfiles)
     {
         # Grab properties to use for informative host output.
         $LocalPath = $UserProfile.LocalPath
@@ -57,13 +74,29 @@ Function CleanProfiles
 
         # If the user profile's local path does not match anything in $AlwaysExcluded, the profile will be removed.
         # This part of the logic does have some gaps, eg account names that don't match their profile folder name.
-        if (!($AlwaysExcluded -like $LocalPath.Replace("C:\Users\",""))) 
+        if (!($AlwaysExcluded -like $LocalPath.Replace("C:\Users\","")))
             {
                 Write-Host "Removing $LocalPath (SID: $ProfileSID)" -ForegroundColor Yellow
-                Try { $UserProfile | Remove-CimInstance -ErrorAction Stop }
-                Catch { Write-Host $Error[0].Exception -ForegroundColor Magenta }
+                Try {
+                    $UserProfile | Remove-CimInstance -ErrorAction Stop
+                }
+                Catch {
+                    Write-Host $Error[0].Exception -ForegroundColor Magenta
+                }
             } # End if
     } # End foreach loop through profiles
-} # End function
+} # End function Remove-StaleProfiles
 
-# Add a switch parameter handler to the script so running it with -Clean will execute the CleanProfiles function. 
+function TranslateSamToSid ($domain, $samaccountname) {
+    # Translate samaccountname to SID; use if getting additional account information from AD, such as special group membership
+    $objUser = New-Object System.Security.Principal.NTAccount($domain,$samaccountname)
+    $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+    $strsid.Value
+} # End function TranslateSamToSid
+
+function TranslateSidToSam ($sid) {
+    # Translate SID to samaccountname; use if getting additional account information from AD, such as special group membership
+    $objSID = New-object System.Security.Principal.SecurityIdentifier($sid)
+    $strUser = $objSID.Translate([System.Security.Principal.NTAccount])
+    $strUser.Value
+} # End function TranslateSidToSam
