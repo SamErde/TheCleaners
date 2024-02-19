214cb0d91512defa87030d25b8caa190a71e00f9
Sam Erde <20478745+SamErde@users.noreply.github.com>
2024-02-19 15:35:12 -0500

    Init placeholder for Invoke-TheCleaners

---
+++ src/TheCleaners/Public/Invoke-TheCleaners.ps1
@@ -0,0 +1,29 @@
+﻿function Invoke-TheCleaners {
+    [CmdletBinding()]
+    param (
+        # Nothing here yet
+    )
+
+    begin {
+        $Logo = @'
+        ╭━━━━┳╮╱╱╱╱╱╭━━━┳╮
+        ┃╭╮╭╮┃┃╱╱╱╱╱┃╭━╮┃┃
+        ╰╯┃┃╰┫╰━┳━━╮┃┃╱╰┫┃╭━━┳━━┳━╮ ╭━━┳━┳━━╮
+        ╱╱┃┃╱┃╭╮┃┃━┫┃┃╱╭┫┃|┃━┫╭╮┃╭╮╮┃|━┫╭┫━━┫
+        ╱╱┃┃╱┃┃┃┃┃━┫┃╰━╯┃╰┫┃━┫╭╮┃||┃┃|━┫|┣━━┃
+        ╱╱╰╯╱╰╯╰┻━━╯╰━━━┻━┻━━┻╯╰┻╯╰┻┻━━┻╯╰━━╯
+
+'@
+    }
+
+    process {
+        Write-Output $Logo
+        Write-Output "The Cleaners are here to help you clean up that crime scene in your log folders."
+        Write-Output "This function hasn't been implemented yet."
+        Get-Command -Module TheCleaners
+    }
+
+    end {
+
+    }
+}
