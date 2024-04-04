696ef26c0eeadd23a02120e46cc03d3ed8230f66
Sam Erde <samuel.erde@gmail.com>
2024-04-04 08:45:07 -0400

    Merge repos

--- src/TheCleaners/Private/Remove-OldFiles.ps1
+++ src/TheCleaners/Private/Remove-OldFiles.ps1
@@ -1,21 +1,21 @@
+<#
+.SYNOPSIS
+    Remove files in a path that are older than the specified number of days.
+
+.DESCRIPTION
+    Remove files in a path that are older than the specified number of days. This function is used by other functions within the module when removing old files.
+
+.EXAMPLE
+    Remove-OldFiles -Path "C:\Windows\Temp" -Days 60 -Recurse
+
+    Removes all files older than 60 does in C:\Windows\Temp with recursion to clean subfolders.
+
+.NOTES
+    Author: Sam Erde
+            https://twitter.com/SamErde
+            https://github.com/SamErde
+#>
 function Remove-OldFiles {
-    <#
-        .SYNOPSIS
-            Remove files in a path that are older than the specified number of days.
-
-        .DESCRIPTION
-            Remove files in a path that are older than the specified number of days. This function is used by other functions within the module when removing old files.
-
-        .EXAMPLE
-            Remove-OldFiles -Path "C:\Windows\Temp" -Days 60 -Recurse
-
-            Removes all files older than 60 does in C:\Windows\Temp with recursion to clean subfolders.
-
-        .NOTES
-            Author: Sam Erde
-                    https://twitter.com/SamErde
-                    https://github.com/SamErde
-    #>
     [CmdletBinding(SupportsShouldProcess)]
     param (
         # The path containing files to remove
