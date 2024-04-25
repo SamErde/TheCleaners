b9f081d92113f215491e0774ed981fd67baeec19
Sam Erde <samuel.erde@gmail.com>
2024-04-24 20:08:13 -0400

    Split functions into their own scripts

--- src/TheCleaners/Public/Remove-OldProfiles.ps1
+++ src/TheCleaners/Public/Remove-OldProfiles.ps1
@@ -27,49 +27,6 @@ function Remove-OldProfiles {
     Remove-OldProfiles
 }
 
-function Get-StaleUserProfiles {
-    <#
-        .SYNOPSIS
-            A script to find old, unused user profiles in Windows.
-
-        .DESCRIPTION
-            THIS IS AN EARLY POC SCRIPT THAT NEEDS TO BE TESTED FOR PROPER EXCLUSIONS. This script finds old, unused
-            profiles in Windows and helps you remove them. It should exclude special accounts and system profiles.
-
-        .EXAMPLE
-            $StaleUserProfiles = Get-StaleUserProfiles -ShowSummary
-
-            Gets stale user profiles into the StaleUserProfiles variable while also showing a summary.
-
-        .NOTES
-            Author: Sam Erde
-                    https://twitter.com/SamErde
-                    https://github.com/SamErde
-
-            Partially inspired by http://woshub.com/delete-old-user-profiles-gpo-powershell/
-    #>
-    [CmdletBinding()]
-    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', 'Get-StaleUserProfiles')]
-    param (
-        [Parameter()]
-        [switch]
-        $ShowSummary
-    )
-
-    # Get all user profiles that have not been used in 60 days, are not currently loaded, and are not special accounts.
-    [array]$StaleUserProfiles = Get-CimInstance -Class Win32_UserProfile | Where-Object { ($_.LastUseTime -lt (Get-Date).AddDays(-60)) -and (!$_.Special) -and (!$_.Loaded) }
-
-    if ($StaleUserProfiles.Count -lt 1 -or !(StaleUserProfiles)) {
-        Write-Information "No stale user profiles were found." -InformationAction Continue
-    } else {
-        if ($ShowSummary) {
-            $StaleUserProfiles | Select-Object LocalPath,SID, @{ Name="Size"; Expression={"{0} MB" -f [math]::Round(((Get-ChildItem $_.LocalPath -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB))} } | Out-Host
-            Write-Information -InformationAction Continue "NOTE: If you do not have access to a profile folder, the size will show as 0 MB."
-        }
-        $StaleUserProfiles
-    }
-}
-
 function Remove-StaleUserProfiles {
     <#
         .SYNOPSIS
@@ -119,45 +76,3 @@ function Remove-StaleUserProfiles {
         } # End if
     } # End foreach loop through profiles
 } # End function Remove-StaleProfiles
-
-function TranslateSamToSid ($domain, $samaccountname) {
-    <#
-        .SYNOPSIS
-            Translates a samaccountname to a SID.
-        .DESCRIPTION
-            Translates a samaccountname to a SID.
-        .PARAMETER domain
-            The domain to search for the samaccountname.
-        .PARAMETER samaccountname
-            The samaccountname to translate to a SID.
-        .EXAMPLE
-            TranslateSamToSid -domain "contoso" -samaccountname "jdoe"
-
-            Translates the samaccountname "jdoe" to a SID.
-    #>
-    [CmdletBinding()]
-
-    $objUser = New-Object System.Security.Principal.NTAccount($domain,$samaccountname)
-    $strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
-    $strsid.Value
-} # End function TranslateSamToSid
-
-function TranslateSidToSam ($sid) {
-    <#
-        .SYNOPSIS
-            Translates a SID to a samaccountname.
-        .DESCRIPTION
-            Translates a SID to a samaccountname.
-        .PARAMETER sid
-            The SID to translate to a samaccountname.
-        .EXAMPLE
-            TranslateSidToSam -sid "S-1-5-21-3623811015-3361044348-30300820"
-
-            Translates the SID to a samaccountname.
-    #>
-    [CmdletBinding()]
-
-    $objSID = New-Object System.Security.Principal.SecurityIdentifier($sid)
-    $strUser = $objSID.Translate([System.Security.Principal.NTAccount])
-    $strUser.Value
-} # End function TranslateSidToSam
