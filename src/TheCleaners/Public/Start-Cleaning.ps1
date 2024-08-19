a14c492f7b8d7d958f2c822a39c941ea9ccc83e1
Sam Erde <samuel.erde@gmail.com>
2024-08-19 14:02:43 -0400

    Clean

--- src/TheCleaners/Public/Start-Cleaning.ps1
+++ src/TheCleaners/Public/Start-Cleaning.ps1
@@ -1,10 +1,10 @@
 ﻿function Start-Cleaning {
     <#
     .SYNOPSIS
-        Calls in The Cleaners to tame the mess in your log folders.
+        Show the commands you can give The Cleaners.
 
     .DESCRIPTION
-        Start the cleaning operation. Put documents in the shredder, clean up the mess in the log folders, and make sure the evidence is gone.
+        Get started with a menu of services The Cleaners can offer.
 
     .PARAMETER NoLogo
         Do not display the logo.
@@ -13,12 +13,6 @@
         Start-Cleaning
 
         View the menu of services that TheCleaners provide.
-
-    .NOTES
-        Author:     Sam Erde
-                    https://twitter.com/SamErde
-                    https://github.com/SamErde
-        Modified:   2024-08-19
 
     .COMPONENT
         TheCleaners
@@ -30,8 +24,7 @@
         $NoLogo
     )
 
-    begin {
-        $Logo = @'
+    $Logo = @'
         ╭━━━━┳╮╱╱╱╱╱╭━━━┳╮
         ┃╭╮╭╮┃┃╱╱╱╱╱┃╭━╮┃┃
         ╰╯┃┃╰┫╰━┳━━╮┃┃╱╰┫┃╭━━┳━━┳━╮ ╭━━┳━┳━━╮
@@ -40,20 +33,13 @@
         ╱╱╰╯╱╰╯╰┻━━╯╰━━━┻━┻━━┻╯╰┻╯╰┻┻━━┻╯╰━━╯
 
 '@
+
+    if (-not $NoLogo) {
+        Write-Output $Logo
     }
-
-    process {
-        if (-not $NoLogo) {
-            Write-Output $Logo
-        }
-        Write-Output "The Cleaners are here to help you clean up your log folders."
-        Get-Command -Module TheCleaners
-        if ( $PSCmdlet.ShouldProcess($Logo) ) {
-            Write-Verbose "This is here because platyPS chokes on the suppression of ShouldProcess."
-        }
-    }
-
-    end {
-
+    Write-Output "The Cleaners are here to help you clean up your log folders."
+    Get-Command -Module TheCleaners
+    if ( $PSCmdlet.ShouldProcess($Logo) ) {
+        Write-Verbose "This is here because platyPS chokes on the suppression of ShouldProcess."
     }
 }
