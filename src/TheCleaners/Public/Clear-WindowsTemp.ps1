c87b43863a04cd268bf976138737a46cbd11a5c7
Sam Erde <samuel.erde@gmail.com>
2024-08-20 05:07:56 -0400

    Change default days. Add ShouldProcess. Update CBH.

--- src/TheCleaners/Public/Clear-WindowsTemp.ps1
+++ src/TheCleaners/Public/Clear-WindowsTemp.ps1
@@ -7,50 +7,49 @@ function Clear-WindowsTemp {
         This script will clean out Windows Temp files older than x days.
 
     .PARAMETER Days
-        The number of days to keep temp files. The default is 60 days.
+        The number of days to keep temp files. The default is 30 days.
 
     .EXAMPLE
         Clear-WindowsTemp -Days 60
 
         Removes all Windows Temp files that are older than 60 days.
 
-    .NOTES
-        Author:     Sam Erde
-                    https://twitter.com/SamErde
-                    https://github.com/SamErde
-        Modified:   2024-08-12
-
     .COMPONENT
         TheCleaners
     #>
-    [CmdletBinding()]
-    #[Alias('Clean-WindowsTemp')]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Low')]
+    [Alias('Clean-WindowsTemp')]
     param (
         # How many days worth of temp files to retain (how far back to filter).
+        [ValidateRange(1, [int16]::MaxValue)] # Ensure it is a positive number.
         [int16]
-        $Days = 60
+        $Days = 30
     )
 
-    begin {
-        $TempPath = Join-Path -Path $env:SystemRoot -ChildPath 'Temp'
-        if (-not (Test-Path -Path $TempPath)) {
-            return
-        }
-        $OldFiles = Get-ChildItem -Path $TempPath -Recurse | Where-Object {
-            $_.LastWriteTime -le ( (Get-Date).AddDays(-$Days) )
-        }
-
-        if ($OldFiles.Count -eq 0) {
-            Write-Output "No files found older than $Days days."
-            return
-        }
+    $TempPath = Join-Path -Path $env:SystemRoot -ChildPath 'Temp'
+    if (-not (Test-Path -Path $TempPath)) {
+        Write-Warning -Message "Unable to find $TempPath."
+        return
+    }
+    $OldFiles = Get-ChildItem -Path $TempPath -Recurse | Where-Object {
+        $_.LastWriteTime -le ( (Get-Date).AddDays(-$Days) )
     }
 
-    process {
-        Remove-OldFiles -Files $OldFiles
+    if ($OldFiles.Count -eq 0) {
+        Write-Output "No files found older than $Days days."
+        return
     }
 
-    end {
-        Write-Output "Removed $($OldFiles.Count) files older than $Days days from the system temp folder."
+    Write-Output "Found $($OldFiles.Count) files and directories older than $Days days in the system temp folder.`n"
+
+    foreach ($file in $OldFiles) {
+        if ( $PSCmdlet.ShouldProcess($file.FullName, 'Remove file') ) {
+            try {
+                Remove-Item -Path $file.FullName -Force -ErrorAction Stop
+                Write-Verbose -Message "Removed file: $($file.FullName)"
+            } catch {
+                Write-Output "  $($Error[-1].Exception.Message)"
+            }
+        }
     }
 }
